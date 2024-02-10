2c26ec31c559e57e5032bd50ae2ce7c05a28035d
MD BAHAUDDIN <56166977+mbahau@users.noreply.github.com>
2024-02-10 22:39:24 +0530

    update

--- main.py
+++ main.py
@@ -12,100 +12,115 @@ from bs4 import BeautifulSoup
 # Point to the local server
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
 
- 
+
+try:
+  currentdate = pd.read_parquet('currentdate.parquet')
+else:
+  currentdate = pd.DataFrame([pd.to_datetime("31-01-2024")], columns = ['Date'] )
+  currentdate['DateM'] = currentdate['Date'].dt.strftime('%d %b %Y')
 
 
-query = 'when is the time of comming Imam Mehdi'
-fetched_urls  = []
-result ="Below information is the google search of : " + query + ". From the below reults, give the recommendation which is(are) the best result(s) i got.\n"
-i=1
-for j in search(query, advanced=True, num_results= 5):
-    # print(j)
-    if "youtube" not in j.url:
-        temp = str(i)+". *URL:"+j.url+" *Title: "+j.title+" *Description: "+j.description+".\n" 
-        fetched_urls.append(j.url)
-        result = result+temp
-        i=i+1
-# result =  result+"\n\n"+" Analyse the above search results, and tell me which one is more related with the query searched . Answer in one word only, just give me the serial number so that i can refer it."
-result =  result+"\n\n"+"Analyse the above search results, and tell me 3 reference which is(are) the relevant result(s). Also give me the provided url(s) of the corresponding result(s). Note: Refer only the provided URLs with this text dont refer any other reference. Put the url inside double quotes"
+query = str(currentdate['DateM'].values[0]) + " top news India"
 
-print(result)
-# ****
-# feeding the input for correct search to nodel 
+currentdate['Date'] = currentdate['Date'] + pd.offsets.Day(-1)
+currentdate['DateM'] = currentdate['Date'].dt.strftime('%d %b %Y')
+currentdate.to_parquet('currentdate.parquet')
 
-systemrole = "answer user query, the best you can serve user"
+for day in range(0,1):
+  fetched_urls  = []
+  result ="Below information is the google search of : " + query + ". From the below reults, give the recommendation which is(are) the best result(s) i got.\n"
+  i=1
+  for j in search(query, advanced=True, num_results= 5):
+      # print(j)
+      if "youtube" not in j.url:
+          temp = str(i)+". *URL:"+j.url+" *Title: "+j.title+" *Description: "+j.description+".\n" 
+          fetched_urls.append(j.url)
+          result = result+temp
+          i=i+1
+  # result =  result+"\n\n"+" Analyse the above search results, and tell me which one is more related with the query searched . Answer in one word only, just give me the serial number so that i can refer it."
+  result =  result+"\n\n"+"Analyse the above search results, and tell me 3 reference which is(are) the relevant result(s). Also give me the provided url(s) of the corresponding result(s). Note: Refer only the provided URLs with this text dont refer any other reference. Put the url inside double quotes"
 
-completion = client.chat.completions.create(
-  model="local-model", # this field is currently unused
-  messages=[
-    {"role": "system", "content": systemrole},
-    {"role": "user", "content": result},
-  ],
-  temperature=0.7,
-)
+  print(result)
+  # ****
+  # feeding the input for correct search to nodel 
 
-print(completion.choices[0].message.content)
+  systemrole = "answer user query, the best you can serve user"
 
+  completion = client.chat.completions.create(
+    model="local-model", # this field is currently unused
+    messages=[
+      {"role": "system", "content": systemrole},
+      {"role": "user", "content": result},
+    ],
+    temperature=0.7,
+  )
 
-recommendation = completion.choices[0].message.content.split("<")
-recommendation
-
-recommendation = [ x for x in recommendation if "http" in x ]
-recommendation
-
-
-recommendation_final = []
-for item in recommendation:
-    recommendation_final.append(item.split(">")[0])
-    # print(item.split(">"))
-recommendation_final
+  print(completion.choices[0].message.content)
 
 
-# recommendation_final[0]
-final_urls = [] 
-for rec_url in recommendation_final:
-    for tu in fetched_urls:
-        print(tu)
-        if rec_url in tu:
-            # print(tu)
-            final_urls.append(tu)
+  recommendation = completion.choices[0].message.content.split("<")
+  recommendation
+
+  recommendation = [ x for x in recommendation if "http" in x ]
+  recommendation
 
 
-i=0
-content  = ""
-for urltmp in final_urls:
-    # Specify the URL of the website you want to scrape
-    url = urltmp  # Replace with the actual URL
-
-    # Send an HTTP GET request to the website
-    response = requests.get(url)
-
-    # Get the HTML content of the page
-    html_content = response.text
-
-    # Parse the HTML using BeautifulSoup
-    soup = BeautifulSoup(html_content, "html.parser")
-    # soup =  BeautifulSoup(s) #parse html with BeautifulSoup
-    td = "Result "+ str(i)+". "+str(soup.text) +".\n"#tag of interest <td>Example information</td>
-    content = content+td
-    i=i+1
-    if(i>2):
-        break
+  recommendation_final = []
+  for item in recommendation:
+      recommendation_final.append(item.split(">")[0])
+      # print(item.split(">"))
+  recommendation_final
 
 
-# systemrole = "answer user query very the best you can serve user"
+  # recommendation_final[0]
+  final_urls = [] 
+  for rec_url in recommendation_final:
+      for tu in fetched_urls:
+          print(tu)
+          if rec_url in tu:
+              # print(tu)
+              final_urls.append(tu)
 
-result = "Below is the News of  "+ query +". Summarise the different News in bullet points. Don't repeat duplicate news. Make it inseresting for user with the facts:\n"
-result =  result+str(" ".join(content.split()))
-result = result+"\n Summarise the News in bullet points."
 
-completion = client.chat.completions.create(
-  model="local-model", # this field is currently unused
-  messages=[
-    # {"role": "system", "content": systemrole},
-    {"role": "user", "content": result},
-  ],
-  temperature=0.7,
-)
+  i=0
+  content  = ""
+  for urltmp in final_urls:
+      # Specify the URL of the website you want to scrape
+      url = urltmp  # Replace with the actual URL
 
-print(completion.choices[0].message.content)
+      # Send an HTTP GET request to the website
+      response = requests.get(url)
+
+      # Get the HTML content of the page
+      html_content = response.text
+
+      # Parse the HTML using BeautifulSoup
+      soup = BeautifulSoup(html_content, "html.parser")
+      # soup =  BeautifulSoup(s) #parse html with BeautifulSoup
+      td = "Result "+ str(i)+". "+str(soup.text) +".\n"#tag of interest <td>Example information</td>
+      content = content+td
+      i=i+1
+      if(i>2):
+          break
+
+
+  # systemrole = "answer user query very the best you can serve user"
+
+  result = "Below is the News of  "+ query +". Summarise the different News in bullet points. Don't repeat duplicate news. Make it inseresting for user with the facts:\n"
+  result =  result+str(" ".join(content.split()))
+  result = result+"\n Summarise the News in bullet points."
+
+  completion = client.chat.completions.create(
+    model="local-model", # this field is currently unused
+    messages=[
+      # {"role": "system", "content": systemrole},
+      {"role": "user", "content": result},
+    ],
+    temperature=0.7,
+  )
+
+  print(completion.choices[0].message.content)
+
+  # saving the LLM model output
+  with open('myfile.txt', 'w') as f:
+    f.write(my_string)
